7195905d958e88bf56b74ffc60993a970acb65d4
zdd <zdd@tuotoo.com>
2022-08-16 16:06:21 +0800

    fix(AddReceiverRequest) remove AddReceiverRequest encrypt

--- services/profitsharing/models.go
+++ services/profitsharing/models.go
@@ -25,7 +25,7 @@ type AddReceiverRequest struct {
 	// 子商户与接收方具体的关系，本字段最多10个字。  当字段relation_type的值为CUSTOM时，本字段必填 当字段relation_type的值不为CUSTOM时，本字段无需填写
 	CustomRelation *string `json:"custom_relation,omitempty"`
 	// 分账接收方类型是MERCHANT_ID时，是商户全称（必传），当商户是小微商户或个体户时，是开户人姓名 分账接收方类型是PERSONAL_OPENID时，是个人姓名（选传，传则校验） 分账接收方类型是PERSONAL_SUB_OPENID时，是个人姓名（选传，传则校验） 1、此字段需要加密，的加密方法详见：敏感信息加密说明 2、使用微信支付平台证书中的公钥 3、使用RSAES-OAEP算法进行加密 4、将请求中HTTP头部的Wechatpay-Serial设置为证书序列号
-	Name *string `json:"name,omitempty" encryption:"EM_APIV3"`
+	Name *string `json:"name,omitempty"`
 	// 子商户与接收方的关系。 本字段值为枚举： SERVICE_PROVIDER：服务商 STORE：门店  STAFF：员工 STORE_OWNER：店主 PARTNER：合作伙伴 HEADQUARTER：总部 BRAND：品牌方 DISTRIBUTOR：分销商 USER：用户 SUPPLIER：供应商 CUSTOM：自定义  * `SERVICE_PROVIDER` - 服务商，  * `STORE` - 门店，  * `STAFF` - 员工，  * `STORE_OWNER` - 店主，  * `PARTNER` - 合作伙伴，  * `HEADQUARTER` - 总部，  * `BRAND` - 品牌方，  * `DISTRIBUTOR` - 分销商，  * `USER` - 用户，  * `SUPPLIER` - 供应商，  * `CUSTOM` - 自定义，
 	RelationType *ReceiverRelationType `json:"relation_type"`
 	// 子商户的公众账号ID，分账接收方类型包含PERSONAL_SUB_OPENID时必填。（直连商户不需要，服务商需要）
